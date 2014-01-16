4b7fbb2c5a548a5de3982daaea718da41bc963a3
gravitano <gue.warsono@gmail.com>
2014-01-16 16:38:32 +0700

    Adding

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	/* Nice Scroll */
+	 $("body").niceScroll({
+	 	touchbehavior:false,
+	 	cursorcolor:"#3388ee",
+	 	cursoropacitymax:0.8,
+	 	cursorwidth:10
+	 });	
+});
